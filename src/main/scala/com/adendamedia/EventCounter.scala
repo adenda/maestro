a8a2e6ff19790df79cdbcf54cff17e1d2a8c2622
Steve King <steve@adendamedia.com>
2017-04-05 15:46:55 -0700

    Separate event counters for channels and patterns

--- src/main/scala/com/adendamedia/EventCounter.scala
+++ src/main/scala/com/adendamedia/EventCounter.scala
@@ -3,9 +3,27 @@ package com.adendamedia
 import akka.agent.Agent
 import akka.actor.ActorSystem
 
-case class StateChannelEventCounter(counter: Int)
+import scala.concurrent.ExecutionContextExecutor
 
-class EventCounter(system: ActorSystem)(implicit val max_val: Int) {
+trait RedisEventCounter
+
+case class StateChannelEventCounter(counter: Int) extends RedisEventCounter
+
+case class StatePatternEventCounter(counter: Int) extends RedisEventCounter
+
+trait EventCounter {
+  implicit val ex: ExecutionContextExecutor
+
+  implicit def eventCounter(num: Int): EventCounterNumber
+
+  val stateAgent: RedisEventCounter
+
+  def incrementCounter: Unit
+
+  def getEventCounterNumber(): RedisEventCounter
+}
+
+class ChannelEventCounter(system: ActorSystem)(implicit val max_val: Int) {
   implicit val ex = system.dispatcher
 
   implicit def eventCounter(num: Int): EventCounterNumber = new EventCounterNumber(num)
@@ -20,3 +38,20 @@ class EventCounter(system: ActorSystem)(implicit val max_val: Int) {
 
   def getEventCounterNumber(): StateChannelEventCounter = stateAgent.get()
 }
+
+class PatternEventCounter(system: ActorSystem)(implicit val max_val: Int) {
+  implicit val ex = system.dispatcher
+
+  implicit def eventCounter(num: Int): EventCounterNumber = new EventCounterNumber(num)
+
+  val stateAgent = Agent(new StatePatternEventCounter(0))
+
+  def incrementCounter: Unit = {
+    stateAgent send (oldState => {
+      oldState.copy(oldState.counter.nextEventNumber)
+    })
+  }
+
+  def getEventCounterNumber(): StatePatternEventCounter = stateAgent.get()
+}
+
